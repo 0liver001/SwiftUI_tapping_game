be5da36014c95ff845ab636f971dd5ea8661c1da
Oliver Kalemba / BBS2H20A <Oliver.Kalemba@edu.bib.de>
2022-01-31 08:21:13 +0100

    Erste Button ansätze - Klicklogik etc.

--- Tapgame/Tapgame/ContentView.swift
+++ Tapgame/Tapgame/ContentView.swift
@@ -7,15 +7,47 @@
 
 import SwiftUI
 
-struct TapButton: View {
+struct TapNumber : View {
     var body: some View {
-        Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+        Text("0")
+            .scaleEffect(5)
+    }
+}
+
+struct TapButtonGUI {
+    var body: some View {
+        Text("Hallo")
+            .padding()
+            .foregroundColor(.white)
+            .background(Color(.blue))
+            .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+            .offset(x:0, y:0)
+    }
+}
+
+struct TapButton: View {
+    //let myGUI : TapButtonGUI
+    var body: some View {
+        Button(action: {
+            self.offset(x:-100)
+        }, label: {
             Text("Hallo")
                 .padding()
                 .foregroundColor(.white)
                 .background(Color(.blue))
                 .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                //.offset(x:0, y:0)
         })
+    }
+}
+
+extension View {
+    //let tb : TapButtonGUI
+    
+    func replace() -> some View {
+        // self.modifier(offset(x:-100))
+        // self.offset(x:CGFloat((Int.random(in: 1..<100))))
+        return self
     }
 }
 
@@ -27,17 +59,24 @@ struct ContentView: View {
         {
             Color(#colorLiteral(red: 0.6981517673, green: 0.9309610128, blue: 0.9120567441, alpha: 1))
             
+            TapNumber()
+            
             VStack
             {
                 Text("The Tap Game")
+                    .bold()
+                    .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
                     .padding(.top, 40)
+                    
                 
                 
                 
+                
+                
+                
+                
+                Spacer()
                 TapButton()
-                
-                
-                
                 Spacer()
                 
             }
